bc2c53ae8dca862eca01958b0fcd517ed37d8e36
shibafu <shibafu528@gmail.com>
2018-11-17 12:04:07 +0900

    add files

---
+++ mikutter_openimg_window_size.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:mikutter_openimg_window_size) do
+
+  UserConfig[:openimg_window_size_width_percent] ||= 70
+  UserConfig[:openimg_window_size_height_percent] ||= 70
+
+  settings('画像プレビュー') do
+    settings('ウィンドウサイズ') do
+      adjustment('幅 (%)', :openimg_window_size_width_percent, 1, 100)
+      adjustment('高さ (%)', :openimg_window_size_height_percent, 1, 100)
+    end
+  end
+  
+end
+
+class Plugin::Openimg::Window
+
+  def default_size
+    @size || [screen.width * (UserConfig[:openimg_window_size_width_percent] / 100.0),
+              screen.height * (UserConfig[:openimg_window_size_height_percent] / 100.0)]
+  end
+  
+end
