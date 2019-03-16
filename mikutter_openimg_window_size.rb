d2fcc40f49aadea7a88576a27d2181b189dff03c
shibafu <shibafu528@gmail.com>
2019-03-16 11:10:26 +0900

    設定グループ名を openimg/.mikutter.yml での機能名表記に合わせた

--- mikutter_openimg_window_size.rb
+++ mikutter_openimg_window_size.rb
@@ -5,7 +5,7 @@ Plugin.create(:mikutter_openimg_window_size) do
   UserConfig[:openimg_window_size_width_percent] ||= 70
   UserConfig[:openimg_window_size_height_percent] ||= 70
 
-  settings('画像プレビュー') do
+  settings('画像ビューア') do
     settings('ウィンドウサイズ') do
       adjustment('幅 (%)', :openimg_window_size_width_percent, 1, 100)
       adjustment('高さ (%)', :openimg_window_size_height_percent, 1, 100)
